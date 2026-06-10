e8dc9bf4bdcc305e903aefa3c417e3e872ef6ea9
Gemma McLean <gemixin@gmail.com>
2026-06-10 11:09:38 +0100

    Changed rounding of thresholds

--- digit_gui.py
+++ digit_gui.py
@@ -21,7 +21,7 @@ MAX_FORCE_LEVEL = 3
 
 # Voltage thresholds (uncomment the relevant thresholds based on which FSR you are using)
 # VOLTAGE_THRESHOLDS = [0.99, 2.99] # FSR 1
-VOLTAGE_THRESHOLDS = [0.92, 3.06]  # FSR 2
+VOLTAGE_THRESHOLDS = [0.91754631, 3.05528719]  # FSR 2
 
 
 class DigitGUI:
